b2db5467040b77eb05713de7b6f0a624ce0d497d
Dreamer <christopherlu.pdx@gmail.com>
2019-10-30 17:00:50 -0700

    function declared, main loop needs work

---
+++ lab.cpp
@@ -0,0 +1,33 @@
+//Lab
+//Chris Lu10/30/19
+//dry run of blind shot
+
+#include <iostream>
+#include <iomanip>
+//not sure what other packets we need
+
+using namespace std;
+
+int sumLoop(int start, int end);
+
+int main(){
+int starting, ending;
+
+	cout << "this program does x" <<  endl;
+	cout << "gimme 2 integers: " << endl;
+	cin >> starting >> ending; 
+
+	cout << "starting is " << starting << endl << "ending is " << ending << endl;
+	sumLoop(1, 3);
+//while ()
+//{
+//}
+
+	return 0;
+}
+
+int sumLoop(int start, int end)
+{
+
+cout << "sumLoop" << endl;
+}
